4b5c42b9f7c196d3768fc773209b672adcecc503
mattia richetto <mattia.richetto@gmail.com>
2018-10-18 21:13:16 -0300

    new API

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,23 +2,49 @@ const { join } = require('path');
 const dirToObject = require('../src');
 
 describe('dirToObject', () => {
-  const scenarios = [
-    {
-      config: { dirPath: join(__dirname, '../__mocks__/foo') },
-      description: 'config w/ dirPath only'
-    },
-    {
-      config: {
-        canAdd: data => data.resolve && data.type,
-        dirPath: join(__dirname, '../__mocks__/foo')
-      },
-      description: 'config w/ canAdd and dirPath'
-    }
-  ];
+  const dirPath = join(__dirname, '../__mocks__/foo');
 
-  scenarios.forEach(({ config, description }) => {
-    it(description, () => {
-      expect(dirToObject(config)).toMatchSnapshot();
+  describe('v1', () => {
+    const scenarios = [
+      {
+        config: { dirPath },
+        description: 'config w/ dirPath only'
+      },
+      {
+        config: {
+          canAdd: data => data.resolve && data.type,
+          dirPath
+        },
+        description: 'config w/ canAdd and dirPath'
+      }
+    ];
+
+    scenarios.forEach(({ config, description }) => {
+      it(description, () => {
+        expect(dirToObject(config)).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('v2', () => {
+    const scenarios = [
+      {
+        description: 'w/ path only',
+        path: dirPath
+      },
+      {
+        description: 'w/ path and options',
+        options: {
+          canAdd: data => data.resolve && data.type
+        },
+        path: dirPath
+      }
+    ];
+
+    scenarios.forEach(({ description, options, path }) => {
+      it(description, () => {
+        expect(dirToObject(path, options)).toMatchSnapshot();
+      });
     });
   });
 });
